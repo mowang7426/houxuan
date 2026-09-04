662be14a8835bf19ee92de18491e1f29734e8243
mowang7426 <972565726@qq.com>
2026-09-04 15:29:45 +0800

    Update RootListController.m

--- KBGlowPrefs/RootListController.m
+++ KBGlowPrefs/RootListController.m
@@ -4,10 +4,10 @@
 
 - (NSArray *)specifiers {
     if (_specifiers == nil) {
-        _specifiers = @[
+        _specifiers = [[NSMutableArray alloc] initWithObjects:
             [PSSpecifier preferenceSpecifierNamed:@"KBGlow 测试" target:nil set:nil get:nil detail:nil cell:PSGroupCell edit:nil],
-            [PSSpecifier preferenceSpecifierNamed:@"如果你看到这行，说明 bundle 加载成功" target:nil set:nil get:nil detail:nil cell:PSStaticTextCell edit:nil]
-        ];
+            [PSSpecifier preferenceSpecifierNamed:@"如果你看到这行，说明 bundle 加载成功" target:nil set:nil get:nil detail:nil cell:PSStaticTextCell edit:nil],
+            nil];
     }
     return _specifiers;
 }
