934b3c972e9f811b6e2e55e14ea3d70e002cd2e5
mowang7426 <972565726@qq.com>
2026-09-04 17:46:20 +0800

    Add files via upload

--- KBGlowManager.m
+++ KBGlowManager.m
@@ -18,7 +18,6 @@ static NSString *const kKBGlowDomain = @"com.mowang.kbglow";
 - (instancetype)init {
     self = [super init];
     if (self) {
-        // 监听设置变化
         [[NSNotificationCenter defaultCenter] addObserver:self
                                                  selector:@selector(reloadSettings)
                                                      name:NSUserDefaultsDidChangeNotification
@@ -33,60 +32,50 @@ static NSString *const kKBGlowDomain = @"com.mowang.kbglow";
 
 - (void)reloadSettings {
     NSUserDefaults *defaults = [[NSUserDefaults alloc] initWithSuiteName:kKBGlowDomain];
-
     self.enabled = [defaults boolForKey:@"enabled"] ?: YES;
-    self.animationType = [defaults integerForKey:@"animationType"] ?: KBGlowAnimationTypeRipple;
+
+    // 动画类型 - 从独立开关读取
+    if ([defaults boolForKey:@"animParticle"]) {
+        self.animationType = KBGlowAnimationTypeParticle;
+    } else if ([defaults boolForKey:@"animGlow"]) {
+        self.animationType = KBGlowAnimationTypeGlow;
+    } else {
+        self.animationType = KBGlowAnimationTypeRipple;
+    }
+
     self.glowSize = [defaults doubleForKey:@"glowSize"] ?: 60.0;
     self.glowDuration = [defaults doubleForKey:@"glowDuration"] ?: 0.6;
     self.glowOpacity = [defaults doubleForKey:@"glowOpacity"] ?: 0.8;
     self.followFinger = [defaults boolForKey:@"followFinger"] ?: YES;
-
     self.wechatEnabled = [defaults boolForKey:@"wechatEnabled"] ?: YES;
     self.baiduEnabled = [defaults boolForKey:@"baiduEnabled"] ?: YES;
     self.sogouEnabled = [defaults boolForKey:@"sogouEnabled"] ?: YES;
 
-    // 颜色 - 优先读取颜色名称
-    NSString *colorName = [defaults stringForKey:@"glowColorName"];
-    if (colorName) {
-        NSDictionary *colorMap = @{
-            @"green":  [UIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:1.0],
-            @"blue":   [UIColor colorWithRed:0.0 green:0.5 blue:1.0 alpha:1.0],
-            @"red":    [UIColor colorWithRed:1.0 green:0.2 blue:0.2 alpha:1.0],
-            @"purple": [UIColor colorWithRed:0.6 green:0.2 blue:1.0 alpha:1.0],
-            @"orange": [UIColor colorWithRed:1.0 green:0.6 blue:0.0 alpha:1.0],
-            @"cyan":   [UIColor colorWithRed:0.0 green:0.9 blue:1.0 alpha:1.0],
-            @"pink":   [UIColor colorWithRed:1.0 green:0.4 blue:0.7 alpha:1.0],
-            @"white":  [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0],
-        };
-        UIColor *mapped = colorMap[colorName];
-        if (mapped) {
-            self.glowColor = mapped;
-        } else {
-            self.glowColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:1.0];
-        }
+    // 颜色 - 从独立开关读取
+    if ([defaults boolForKey:@"colorWhite"]) {
+        self.glowColor = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
+    } else if ([defaults boolForKey:@"colorPink"]) {
+        self.glowColor = [UIColor colorWithRed:1.0 green:0.4 blue:0.7 alpha:1.0];
+    } else if ([defaults boolForKey:@"colorCyan"]) {
+        self.glowColor = [UIColor colorWithRed:0.0 green:0.9 blue:1.0 alpha:1.0];
+    } else if ([defaults boolForKey:@"colorOrange"]) {
+        self.glowColor = [UIColor colorWithRed:1.0 green:0.6 blue:0.0 alpha:1.0];
+    } else if ([defaults boolForKey:@"colorPurple"]) {
+        self.glowColor = [UIColor colorWithRed:0.6 green:0.2 blue:1.0 alpha:1.0];
+    } else if ([defaults boolForKey:@"colorRed"]) {
+        self.glowColor = [UIColor colorWithRed:1.0 green:0.2 blue:0.2 alpha:1.0];
+    } else if ([defaults boolForKey:@"colorBlue"]) {
+        self.glowColor = [UIColor colorWithRed:0.0 green:0.5 blue:1.0 alpha:1.0];
     } else {
-        // 兼容旧版：读取颜色数组
-        NSArray *colorComponents = [defaults arrayForKey:@"glowColor"];
-        if (colorComponents && colorComponents.count >= 3) {
-            CGFloat r = [colorComponents[0] floatValue];
-            CGFloat g = [colorComponents[1] floatValue];
-            CGFloat b = [colorComponents[2] floatValue];
-            CGFloat a = colorComponents.count >= 4 ? [colorComponents[3] floatValue] : 1.0;
-            self.glowColor = [UIColor colorWithRed:r green:g blue:b alpha:a];
-        } else {
-            self.glowColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:1.0];
-        }
+        self.glowColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.0 alpha:1.0];
     }
 }
 
 - (BOOL)isCurrentKeyboardEnabled {
     if (!self.enabled) return NO;
-
     NSString *bundleID = [[NSBundle mainBundle] bundleIdentifier];
     if (!bundleID) return NO;
-
     NSString *lower = [bundleID lowercaseString];
-
     if ([lower containsString:@"wechat"] || [lower containsString:@"wcinput"]) {
         return self.wechatEnabled;
     }
@@ -96,20 +85,15 @@ static NSString *const kKBGlowDomain = @"com.mowang.kbglow";
     if ([lower containsString:@"sogou"] || [lower containsString:@"sohu"]) {
         return self.sogouEnabled;
     }
-
     return NO;
 }
 
 - (BOOL)isKeyView:(UIView *)view {
     if (!view) return NO;
-
-    // 向上遍历3层，检查类名是否包含按键关键词
     UIView *current = view;
     for (NSInteger i = 0; i < 4 && current; i++) {
         NSString *className = NSStringFromClass([current class]);
         NSString *lower = [className lowercaseString];
-
-        // 排除明显不是按键的类
         if ([lower containsString:@"textfield"] ||
             [lower containsString:@"textview"] ||
             [lower containsString:@"scrollview"] ||
@@ -117,8 +101,6 @@ static NSString *const kKBGlowDomain = @"com.mowang.kbglow";
             [lower containsString:@"collectionview"]) {
             return NO;
         }
-
-        // 匹配按键类名
         if ([lower containsString:@"key"] ||
             [lower containsString:@"button"] ||
             [lower containsString:@"keyview"] ||
@@ -126,10 +108,8 @@ static NSString *const kKBGlowDomain = @"com.mowang.kbglow";
             [lower containsString:@"keyboardkey"]) {
             return YES;
         }
-
         current = current.superview;
     }
-
     return NO;
 }
 
@@ -146,24 +126,18 @@ static NSString *const kKBGlowDomain = @"com.mowang.kbglow";
         }
         current = current.superview;
     }
-    return view; // 找不到就用原 view
+    return view;
 }
 
 - (void)triggerGlowInView:(UIView *)view atPoint:(CGPoint)point {
     if (!self.enabled || !view) return;
     if (![self isCurrentKeyboardEnabled]) return;
-
     UIView *keyView = [self findKeyViewFromView:view];
     UIView *container = keyView.superview ?: keyView;
-
-    // 转换坐标到 container
     CGPoint convertedPoint = [keyView convertPoint:point toView:container];
     if (!self.followFinger) {
-        // 不跟随手指时，用按键中心
         convertedPoint = CGPointMake(CGRectGetMidX(keyView.frame), CGRectGetMidY(keyView.frame));
     }
-
-    // 创建发光视图
     KBGlowView *glowView = [[KBGlowView alloc] initWithFrame:container.bounds];
     glowView.glowColor = self.glowColor;
     glowView.glowSize = self.glowSize;
@@ -171,7 +145,6 @@ static NSString *const kKBGlowDomain = @"com.mowang.kbglow";
     glowView.glowOpacity = self.glowOpacity;
     glowView.animationType = self.animationType;
     glowView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-
     [container addSubview:glowView];
     [glowView startAnimationAtPoint:convertedPoint];
 }
