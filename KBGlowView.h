eaec9159a22b46c98cdd2f9faf3a435b4233131d
mowang7426 <972565726@qq.com>
2026-09-05 12:09:47 +0800

    Add files via upload

--- KBGlowView.h
+++ KBGlowView.h
@@ -1,6 +1,6 @@
 #import <UIKit/UIKit.h>
 typedef NS_ENUM(NSInteger, KBGlowAnimationType){ KBGlowAnimationTypeRipple=0, KBGlowAnimationTypeGlow=1, KBGlowAnimationTypeParticle=2 };
-@interface KBGlowView:UIView
+@interface KBGlowView:UIView <CAAnimationDelegate>
 @property(nonatomic,strong)UIColor *glowColor;
 @property(nonatomic,assign)CGFloat glowSize;
 @property(nonatomic,assign)CGFloat glowDuration;
