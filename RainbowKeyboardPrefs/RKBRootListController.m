8bf296a8ae59c8e12dfbc5fc83a32ef173a09252
mowang7426 <972565726@qq.com>
2026-09-20 08:36:24 +0800

    Add files via upload

--- RainbowKeyboardPrefs/RKBRootListController.m
+++ RainbowKeyboardPrefs/RKBRootListController.m
@@ -1,12 +1,6 @@
 #import <UIKit/UIKit.h>
-
-// PreferenceLoader/Preferences.framework 提供 PSListController。
-// 这里仅声明本项目实际使用的 API，避免依赖本地 SDK 的私有 Preferences 头文件。
-@interface PSListController : UITableViewController
-- (NSArray *)loadSpecifiersFromPlistName:(NSString *)name target:(id)target;
-@end
-
-static NSString * const RKPrefsPath = @"/var/mobile/Library/Preferences/com.minis.rainbowkeyboard.plist";
+#import <Preferences/PSListController.h>
+#import <Preferences/PSSpecifier.h>
 
 @interface RKBRootListController : PSListController
 @end
@@ -14,11 +8,10 @@ static NSString * const RKPrefsPath = @"/var/mobile/Library/Preferences/com.mini
 @implementation RKBRootListController
 
 - (NSArray *)specifiers {
-    // 使用标准 PreferenceBundle 的 Root plist 方式，让 Preferences.app 自己创建
-    // PSSwitchCell / PSSliderCell 等控件。这样也避免手工 UITableView 与
-    // PSListController 内部 specifier 生命周期不一致的问题。
-    NSArray *specifiers = [self loadSpecifiersFromPlistName:@"RainbowKeyboard" target:self];
-    return specifiers ?: @[];
+    if (!_specifiers) {
+        _specifiers = [[self loadSpecifiersFromPlistName:@"RainbowKeyboard" target:self] retain];
+    }
+    return _specifiers;
 }
 
 - (void)viewDidLoad {
@@ -26,17 +19,14 @@ static NSString * const RKPrefsPath = @"/var/mobile/Library/Preferences/com.mini
     self.title = @"彩虹键盘光效";
 }
 
-// 保留一个简单的默认配置入口，实际开关/滑块由 RainbowKeyboard.plist 负责保存。
-// Tweak 读取同一个 plist，因此无需在这里重复实现控件。
-- (void)setPreferenceValue:(id)value forKey:(NSString *)key {
-    NSMutableDictionary *prefs = [NSMutableDictionary dictionaryWithContentsOfFile:RKPrefsPath];
-    if (!prefs) prefs = [NSMutableDictionary dictionary];
-    if (value) prefs[key] = value;
-    else [prefs removeObjectForKey:key];
-    [prefs writeToFile:RKPrefsPath atomically:YES];
-    CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(),
-                                          CFSTR("com.minis.rainbowkeyboard.changed"),
-                                          NULL, NULL, YES);
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier {
+    [super setPreferenceValue:value specifier:specifier];
+    NSString *notification = [specifier propertyForKey:@"PostNotification"];
+    if (notification.length) {
+        CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(),
+                                              (__bridge CFStringRef)notification,
+                                              NULL, NULL, YES);
+    }
 }
 
 @end
