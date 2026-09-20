4164e305a62efe23cb9732b7b5bfac343163650f
mowang7426 <972565726@qq.com>
2026-09-20 15:46:09 +0800

    Add files via upload

--- RainbowKeyboardPrefs/RKBRootListController.m
+++ RainbowKeyboardPrefs/RKBRootListController.m
@@ -20,12 +20,29 @@ static NSString * const RKPath = @"/var/mobile/Library/Preferences/com.minis.rai
     if (!key || !value) return;
     NSMutableDictionary *values = [[NSDictionary dictionaryWithContentsOfFile:RKPath] mutableCopy] ?: [NSMutableDictionary dictionary];
     values[key] = value;
+    if ([key isEqualToString:@"Preset"]) {
+        NSInteger preset = [value integerValue];
+        NSArray *options = @[
+            @{@"Opacity":@.4,@"Brightness":@.8,@"Duration":@.6,@"Spread":@1.5,@"Softness":@10,@"CoreStrength":@.3,@"MaxEffects":@3},
+            @{@"Opacity":@.75,@"Brightness":@1,@"Duration":@.55,@"Spread":@2.2,@"Softness":@8,@"CoreStrength":@.65,@"MaxEffects":@4},
+            @{@"Opacity":@.6,@"Brightness":@.95,@"Duration":@.25,@"Spread":@1.3,@"Softness":@5,@"CoreStrength":@.6,@"MaxEffects":@3},
+            @{@"Opacity":@.65,@"Brightness":@.95,@"Duration":@.55,@"Spread":@2,@"Softness":@8,@"CoreStrength":@.5,@"MaxEffects":@4}
+        ];
+        if (preset >= 0 && preset < (NSInteger)options.count) {
+            [values addEntriesFromDictionary:options[preset]];
+            values[@"ColorMode"] = @0;
+            values[@"BackgroundFeedback"] = @YES;
+            values[@"BackgroundStrength"] = @.18;
+            values[@"BackgroundDuration"] = @.4;
+        }
+    } else values[@"Preset"] = @(-1);
     if (![values writeToFile:RKPath atomically:YES]) {
         UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"保存失败" message:@"配置文件未写入，请检查偏好设置目录权限。" preferredStyle:UIAlertControllerStyleAlert];
         [alert addAction:[UIAlertAction actionWithTitle:@"知道了" style:UIAlertActionStyleDefault handler:nil]];
         [self presentViewController:alert animated:YES completion:nil];
         return;
     }
+    [self reloadSpecifiers];
     CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), CFSTR("com.minis.rainbowkeyboard.changed"), NULL, NULL, YES);
 }
 @end
